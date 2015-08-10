e1f5dabe532f0242ffbab69c1e6f794a1945ed08
Stuart Auld <sauld@cozero.com.au>
2015-08-10 11:26:28 +1000

    correct name for js/css assets

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require 'sinatra/asset_pipeline'
 
 class App < Sinatra::Base
   configure do
-    set :assets_precompile, %w(application.js application.css *.png *.jpg *.svg *.eot *.ttf *.woff)
+    set :assets_precompile, %w(app.js app.css *.png *.jpg *.svg *.eot *.ttf *.woff)
     set :assets_css_compressor, :sass
     set :assets_js_compressor, :uglifier
     register Sinatra::AssetPipeline
