0a16f33e34641d3a3e94a8505dcc2f88feea120a
Stuart Auld <sauld@cozero.com.au>
2015-09-24 18:50:53 +1000

    remove some commentary and add phantom user add

--- helpers/auth.rb
+++ helpers/auth.rb
@@ -1,9 +1,6 @@
 module Auth
   def build_user
     if @user.nil? && !session['email'].nil?
-      # @user = {}
-      # @user['email']  = session['email']
-      # Create the user if they don't exist
       if User.where(email: session['email']).count == 0
         User.create(
           name: session['name'],
@@ -14,15 +11,8 @@ module Auth
         )
         flash[:notice] = "New user successfully created for #{session[:email]}"
       end
-      puts "email: #{session['email']}"
       @user = User.where(email: session['email']).first
       puts "User: #{@user.inspect}"
-      # @user['name']   = session['name']
-      # @user['id']     = session['id']
-    # elsif ENV['RACK_ENV'] != 'production'
-    #   @user['name']   = "Stuart Auld"
-    #   @user['email']  = "sauld@cozero.com.au"
-    #   @user['id']     = "sauld@cozero.com.au"
     end
     @user
   end
